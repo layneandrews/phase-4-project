6d40d80fe737300ca3c7d9d0886b62b53e7b9b29
Layne <saxalayne@gmail.com>
2023-07-19 09:17:31 -0600

    updated app.py and models.py

--- server/models.py
+++ server/models.py
@@ -1,16 +1,21 @@
 # import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
+# serializer formats our data
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
 
 # initializes our DB to link up with SQLAlchemy
 db = SQLAlchemy()
 
 # this creates a table in our DB
-class Property(db.Model):
+class Property(db.Model, SerializerMixin):
     # defines the name of the table
     __tablename__ = "properties"
 
     id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String)
+    address = db.Column(db.String, nullable=False)
+    city = db.Column(db.String, nullable=False)
+    state = db.Column(db.String, nullable=False)
     image = db.Column(db.String)
     bedrooms = db.Column(db.Integer)
     bathrooms = db.Column(db.Integer)
@@ -23,6 +28,15 @@ class Property(db.Model):
     # keeps track of when a property is updated
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    # takes tuple of all columns you want to omit
+    serialize_rules = ('-created_at', '-updated_at')
+
+    @validates("address", "city", "state")
+    def val_location(self, key, value):
+        if isinstance(value, str) and len(value) in range(2, 51):
+            return value
+        raise ValueError("Exceeded max characters allowed for address")
+
     def __repr__(self):
-        return f'<Property Address:{self.address}, Image:{self.image}, Bedrooms: {self.bedrooms}, Bathrooms: {self.bathrooms}, Floors: {self.floors}, Garage: {self.garage}, Pool: {self.pool}>'
+        return f'<Property Address:{self.address}, City:{self.city}, State:{self.state}, Image:{self.image}, Bedrooms: {self.bedrooms}, Bathrooms: {self.bathrooms}, Floors: {self.floors}, Garage: {self.garage}, Pool: {self.pool}>'
 
